dc29c2f3bc777e8f98eabd899e189a41d62a9713
abderrahmane abdelioua <dahmano66@gmail.com>
2023-02-05 04:37:00 +0300

    adding guides and styling guides page

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,7 +1,17 @@
-exports.handler = async () => {
+exports.handler = async (event, context) => {
   console.log("function ran");
 
-  const data = { name: "mario", age: 26, job: "plumber" };
+  const data = [
+    { title: "Beat All zelda Bosses Like a Boss", author: "mario" },
+    { title: "Mario Kart Shortcuts You Never Knew Existed", author: "luigi" },
+    { title: "Ultimate Street Fighter Guide", author: "Chun-li" },
+  ];
+  if (!context.clientContext.user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ msg: "You Must Be Logged in!!" }),
+    };
+  }
 
   return {
     statusCode: 200,
--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,64 @@
-import styles from '../styles/Guides.module.css'
+import AuthContext from "@/store/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import styles from "../styles/Guides.module.css";
 
 export default function Guides() {
+  const [guides, setGuides] = useState(null);
+  const [error, setError] = useState(null);
+
+  const { user, authReady } = useContext(AuthContext);
+  useEffect(() => {
+    if (authReady) {
+      fetch(
+        "/.netlify/functions/supermario",
+        user && {
+          headers: {
+            Authorization: "Bearer " + user.token.access_token,
+          },
+        }
+      )
+        .then((data) => {
+          if (!data.ok) {
+            throw Error("You Must Be Logged in to View this Page!!");
+          }
+          return data.json();
+        })
+        .then((data) => {
+          setGuides(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGuides(null);
+        });
+    }
+  }, [user, authReady]);
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
-    </div> 
-  )
+
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides &&
+        guides.map((guide) => (
+          <div key={guide.title} className={styles.card}>
+            <h3>{guide.title}</h3>
+            <h4>Written By {guide.author}</h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+              amet. Ab aliquid enim voluptatem officia error, labore cum nobis
+              debitis soluta qui aut excepturi maxime eaque velit dolorum sit
+              autem!
+            </p>
+          </div>
+        ))}
+    </div>
+  );
 }
